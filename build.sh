d17210e7d85afe69ca8e9647369befcbb0f7c170
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-04 14:17:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 wget http://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 xz -d gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 tar xvf gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar
-export PATH="$PATH:~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin"
-sudo update-alternatives --install /usr/bin/gcc gcc ~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc 70
-sudo update-alternatives --install /usr/bin/gcc-ar gcc-ar ~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc-ar 70
+export PATH="$PATH:~/mu_platforms_oneplus6t/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin"
+sudo update-alternatives --install /usr/bin/gcc gcc ~/mu_platforms_oneplus6t/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc 70
+sudo update-alternatives --install /usr/bin/gcc-ar gcc-ar ~/mu_platforms_oneplus6t/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc-ar 70
 cd ..
 mkdir venv
 python3 -m venv BuildMuVenv
 source BuildMuVenv/bin/activate
 cd mu_platforms_oneplus6t
 pip install --upgrade -r pip-requirements.txt
-stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=VS2019
-stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=VS2019
-stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=VS2019
+stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
+stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
+stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
 mkdir img
 cp Build/OnePlus6TPkg/DEBUG_VS2019/FV/ONEPLUS6TPKG_UEFI.fd ./img
 cp dtb/fajita.dtb ./img
