8f30e379c56788fa302a5f4470ddb952ad9b956e
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-04 14:25:43 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ python3 -m venv BuildMuVenv
 source BuildMuVenv/bin/activate
 cd mu_platforms_oneplus6t
 pip install --upgrade -r pip-requirements.txt
-stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
-stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
-stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=GCC
+stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
+stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
 mkdir img
-cp Build/OnePlus6TPkg/DEBUG_VS2019/FV/ONEPLUS6TPKG_UEFI.fd ./img
+cp Build/OnePlus6TPkg/DEBUG_CLANG38/FV/ONEPLUS6TPKG_UEFI.fd ./img
 cp dtb/fajita.dtb ./img
 echo > ./img/ramdisk
 cd img
