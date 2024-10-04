431191f103870b096596e42e8d62b20aa0087909
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-04 14:15:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 wget http://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 xz -d gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 tar xvf gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar
-export PATH="$PATH:ToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin"
-sudo update-alternatives --install /usr/bin/gcc gcc ToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc 70
-sudo update-alternatives --install /usr/bin/gcc-ar gcc-ar ToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc-ar 70
+export PATH="$PATH:~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin"
+sudo update-alternatives --install /usr/bin/gcc gcc ~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc 70
+sudo update-alternatives --install /usr/bin/gcc-ar gcc-ar ~/MuToolChain/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc-ar 70
 cd ..
 mkdir venv
 python3 -m venv BuildMuVenv
