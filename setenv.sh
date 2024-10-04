1a48f3d22b2843b601741cab31b28fc6934287b2
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-04 15:31:48 +0200

    Update and rename build.sh to setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -10,19 +10,6 @@ python3 -m venv BuildMuVenv
 source BuildMuVenv/bin/activate
 cd mu_platforms_oneplus6t
 pip install --upgrade -r pip-requirements.txt
-export CLANG38_CC_FLAGS="-Wno-error=unaligned-access -Wunused-but-set-variable"
-stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-mkdir img
-cp Build/OnePlus6TPkg/DEBUG_CLANG38/FV/ONEPLUS6TPKG_UEFI.fd ./img
-cp dtb/fajita.dtb ./img
-echo > ./img/ramdisk
-cd img
-gzip -c < ./ONEPLUS6TPKG_UEFI.fd > "./uefi-fajita.img.gz"
-cat "./uefi-fajita.img.gz" "./fajita.dtb" > "./uefi-fajita.img.gz-dtb"
-abootimg --create "./boot-fajita.img" -k "./uefi-fajita.img.gz-dtb" -r ramdisk
-
 
 
 
