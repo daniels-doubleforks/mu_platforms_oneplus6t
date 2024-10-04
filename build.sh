85ffb33ce9a6898cad9ee7b7838c7baeb57bbff5
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-04 15:34:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 export CLANG38_CC_FLAGS="-Wno-error=unaligned-access -Wunused-but-set-variable"
-stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38
-mkdir img
-cp Build/OnePlus6TPkg/DEBUG_CLANG38/FV/ONEPLUS6TPKG_UEFI.fd ./img
-cp dtb/fajita.dtb ./img
-echo > ./img/ramdisk
-cd img
-gzip -c < ./ONEPLUS6TPKG_UEFI.fd > "./uefi-fajita.img.gz"
-cat "./uefi-fajita.img.gz" "./fajita.dtb" > "./uefi-fajita.img.gz-dtb"
-abootimg --create "./boot-fajita.img" -k "./uefi-fajita.img.gz-dtb" -r ramdisk
-cp boot-fajita.img ../
+stuart_setup -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38||exit 1
+stuart_update -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38||exit 1
+stuart_build -c Platforms/OnePlus6TPkg/PlatformCI/PlatformBuild.py TOOL_CHAIN_TAG=CLANG38||exit 1
+mkdir img||exit 1
+cp Build/OnePlus6TPkg/DEBUG_CLANG38/FV/ONEPLUS6TPKG_UEFI.fd ./img||exit 1
+cp dtb/fajita.dtb ./img||exit 1
+echo > ./img/ramdisk||exit 1
+cd img||exit 1
+gzip -c < ./ONEPLUS6TPKG_UEFI.fd > "./uefi-fajita.img.gz"||exit 1
+cat "./uefi-fajita.img.gz" "./fajita.dtb" > "./uefi-fajita.img.gz-dtb"||exit 1
+abootimg --create "./boot-fajita.img" -k "./uefi-fajita.img.gz-dtb" -r ramdisk||exit 1
+cp boot-fajita.img ../||exit 1
